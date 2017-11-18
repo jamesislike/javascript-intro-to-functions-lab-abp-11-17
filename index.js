d6403a86c6ae8568647c66a10683577f6575df77
James Gomis <jamesislike@gmail.com>
2017-11-18 01:12:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,11 +5,3 @@ shout = function shout(string) {
 function whisper(string){
   return string.toLowerCase()
 }
-
-function logShout(string){
-  console.log().toUpperCase()
-}
-
-function logWhisper(string){
-  console.log.().toLowerCase()
-}
